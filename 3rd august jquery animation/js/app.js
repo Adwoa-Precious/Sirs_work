7f14fb0dfa1dd3e71ad48f718dea35efb53a74bf
Adwoa-Precious <favourdorothy2017@gmail.com>
2020-08-12 14:54:23 +0000

    Create app.js

---
+++ 3rd august jquery animation/js/app.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+
+    // $('.box').click(function(){
+
+    //     $('.box').css('bckground-color','green')
+        
+        
+        
+    //     $('.box').animate({
+    //         left:'400px',
+    //         top:'400px',
+    //         width:'50px',
+    //         height:'50px',
+    //     }, 6000)
+    // })
+
+
+    // $('.box').hover(function(){
+    //     $('.box').stop().animate({
+    //         left:'500px',
+    //         top:'400px',
+    //         width:'50px',
+    //         height:'50px'
+    //     },800
+            
+    //     )
+    // })
+
+    // $('.fadein').click(function(){
+    //     $('.box').FadeIn(1000, function(){
+    //     });
+    // });
+
+    // $('.fadeout').click(function(){
+    //     $('.box').FadeOut(1000, function(){
+    //     });
+    // });
+
+    // $('.fadetoggle').click(function(){
+    //     $('.box').FadeToggle(1000, function(){
+    //     });
+    // });
+
+
+
+    $('.button').hover(function(){
+        let dat=new Date;
+        $('.box').see('slow', function(){
+
+        })
+    })
+    
+})
